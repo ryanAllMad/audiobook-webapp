f152781df5fe76a15207344339485e1a8813cbe8
ryanAllMad <ryan@allmaddesigns.com>
2020-01-31 13:22:56 -0800

    Initial Commit for Audiobook Web App

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+    function showThis(el) {
+        document.querySelector(el).style.display = "inherit";
+    }
+    function hideThis(el) {
+        document.querySelector(el).style.display = "none";
+    }
+
+
