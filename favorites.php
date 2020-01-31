9b7333397d0bf4f54b75eeaa6b1aa833941d311b
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2020-01-31 13:54:16 -0800

    Switching over to php files
    For templating and to set the stage for cookies/sessions and form interaction

---
+++ favorites.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+<body class="favorites-page">
+  <div class="container">
+    <!--landmark banner area-->
+    <header class="search-box">
+      <form role="search">
+        <!--Search Aria Role-->
+        <input type="search" aria-label="search text" name="search" />
+        <button type="submit" value="Search">
+          Search<span class="small icon"><img src="http://audiobook/assets/search-solid.svg" /></span>
+        </button>
+      </form>
+    </header>
+    <div class="filter">
+      <div class="filt small icon">
+        <img src="http://audiobook/assets/filter-solid.svg" alt="filter results" /><br />Filter
+      </div>
+      <div class="sort small icon">
+        <img src="http://audiobook/assets/sort-alpha-down-solid.svg" alt="sort results" /><br />Sort
+      </div>
+    </div>
+        <!--main content area landmark-->
+    <main class="content"></main>
+<?php include 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,53 @@
+<!--contentinfo area landmark-->
+<footer class="footer-nav">
+      <!--navigation area landmark-->
+      <nav>
+        <ul>
+          <li>
+            <div>
+              <a href="http://audiobook/home.php" title="home"><span><img src="http://audiobook/assets/home-solid.svg" alt="to home" /></span></a>
+            </div>
+            <a href="http://audiobook/home.php" title="home">Home</a>
+          </li>
+          <li>
+            <div>
+                <a href="http://audiobook/new-releases.php" title="new releases">
+                <span><img src="http://audiobook/assets/glasses-solid.svg" alt="to new releases" /></span></a>
+            </div>
+            <a href="http://audiobook/new-releases.php" title="new releases"> New</a>
+          </li>
+          <li>
+            <div>
+              <a href="http://audiobook/checked-out.php" title="checked out"><span><img
+                    src="http://audiobook/assets/check-solid.svg" alt="to checked out page" /></span></a>
+            </div>
+            <a href="http://audiobook/checked-out.php" title="checked out">Checked Out</a>
+          </li>
+          <li>
+            <div>
+              <a href="http://audiobook/now-playing.php" title="now playing"><span><img
+                    src="http://audiobook/assets/caret-square-right-regular.svg" alt="to now playing page" /></span></a>
+            </div>
+            <a href="http://audiobook/now-playing.php" title="now playing">Now Playing</a>
+          </li>
+          <li>
+            <div>
+              <a href="http://audiobook/search.php" title="search"><span><img
+                    src="http://audiobook/assets/search-solid.svg" alt="to search" /></span></a>
+            </div>
+            <a href="http://audiobook/search.php" title="search">Search</a>
+          </li>
+          <li>
+            <div>
+              <a href="http://audiobook/settings.php" title="settings">
+                <span><img src="http://audiobook/assets/cog-solid.svg" alt="to app settings" /></span></a>
+            </div>
+            <a href="http://audiobook/settings.php" title="settings">Settings</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  </div>
+</body>
+
+</html>
